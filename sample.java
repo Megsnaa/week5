13bc0e78d94eb786ff88fd71cce2d9997f79b1c3
Megsnaa <131870158+Megsnaa@users.noreply.github.com>
2025-07-31 14:52:13 +0530

    fifth commit

--- sample.java
+++ sample.java
@@ -2,5 +2,6 @@ class sample{
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("HIIII NANDUUUUUUUU");
+		System.out.println("scm polling to monitor change commits in git hub!");
 	}
 }
